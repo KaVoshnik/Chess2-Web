0aaf997b4c6a4dae9e6f9dcea60ba939b715bd63
kavoshnik <hikkan008@gmail.com>
2025-01-29 23:01:58 +0700

    General fix

--- js/server.js
+++ js/server.js
@@ -19,38 +19,39 @@ app.use(cors());
 app.use(express.json());
 
 app.post('/register', async (req, res) => {
-  const { username, password } = req.body;
+  const { username, password, email } = req.body;
   const hashedPassword = await bcrypt.hash(password, 10);
 
   try {
-    const result = await pool.query('INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *', [username, hashedPassword]);
-    res.status(201).json({ id: result.rows[0].id, username: result.rows[0].username });
+    const result = await pool.query(
+      'INSERT INTO users (username, password, email) VALUES ($1, $2, $3) RETURNING *',
+      [username, hashedPassword, email]
+    );
+    res.status(201).json({ id: result.rows[0].user_id, username: result.rows[0].username });
   } catch (error) {
-    res.status(400).json({ error: 'User with this name found, do you want login?' });
+    console.error('Error during registration:', error);
+    res.status(400).json({ error: 'User with this name found, do you want to login?' });
   }
-  res.json({ success: true });  
 });
 
 app.post('/login', async (req, res) => {
   const { username, password } = req.body;
 
-  const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
-  if (user.rows.length > 0) {
-    const isMatch = await bcrypt.compare(password, user.rows[0].password);
-    if (isMatch) {
-      const token = jwt.sign({ id: user.rows[0].id }, 'your_jwt_secret', { expiresIn: '1h' });
-      return res.json({ token });
+  try {
+    const user = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
+    if (user.rows.length > 0) {
+      const isMatch = await bcrypt.compare(password, user.rows[0].password);
+      if (isMatch) {
+        const token = jwt.sign({ id: user.rows[0].user_id }, 'your_jwt_secret', { expiresIn: '1h' });
+        return res.json({ token });
+      }
     }
+    res.status(401).json({ error: 'Incorrect username or password.' });
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).json({ error: 'Server error' });
   }
-  res.json({ success: true });
-  res.status(401).json({ error: 'Incorrect username or password.' });
 });
-
-if (data.success) {
-  localStorage.setItem('username', username);
-  window.location.href = 'user_profile.html';
-}
-
 
 app.listen(port, () => {
   console.log(`The server is running on http://localhost:${port}`);
