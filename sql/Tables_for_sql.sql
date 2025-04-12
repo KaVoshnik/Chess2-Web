f697d6683c58ce2d82fbd98df3f69035059defd2
Kirill <112951242+KaVoshnik@users.noreply.github.com>
2025-04-12 08:48:01 +0700

    Update Tables_for_sql.sql

--- sql/Tables_for_sql.sql
+++ sql/Tables_for_sql.sql
@@ -1,27 +1,29 @@
 CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY,         -- Уникальный идентификатор пользователя
-    username VARCHAR(50) NOT NULL UNIQUE, -- Имя пользователя
-    email VARCHAR(100) NOT NULL UNIQUE, -- Email пользователя
-    password VARCHAR(255) NOT NULL,    -- Хэшированный пароль
-    created_at TIMESTAMP DEFAULT NOW() -- Дата создания пользователя
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT NOW()
 );
 
--- Таблица для хранения данных о шахматных игроках
 CREATE TABLE chess_players (
-    player_id SERIAL PRIMARY KEY,       -- Уникальный идентификатор игрока
-    user_id INT REFERENCES users(user_id) ON DELETE CASCADE, -- Связь с таблицей users
-    wins INT DEFAULT 0,                 -- Количество побед
-    losses INT DEFAULT 0,               -- Количество поражений
-    total_games INT DEFAULT 0,          -- Общее количество игр
-    win_rate DECIMAL(5, 2) DEFAULT 0.00, -- Процент побед
-    cr DECIMAL(10, 2) DEFAULT 0.00      -- Рейтинг игрока (CR)
+    player_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(user_id) ON DELETE CASCADE,
+    wins INT DEFAULT 0,
+    losses INT DEFAULT 0,
+    total_games INT DEFAULT 0,
+    win_rate DECIMAL(5, 2) DEFAULT 0.00,
+    cr DECIMAL(10, 2) DEFAULT 0.00
 );
+
+-- Alters
 
 ALTER TABLE chess_players
 ADD COLUMN rank INT DEFAULT NULL;
 
 ALTER TABLE chess_players
 ADD COLUMN draws INT DEFAULT 0;
+
+-- Views
 
 CREATE OR REPLACE VIEW chess_players_with_rank AS
 SELECT 
